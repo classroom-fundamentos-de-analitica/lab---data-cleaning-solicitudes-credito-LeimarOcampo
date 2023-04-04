f8ce45358d5f2cbe3ff73c89fbd69013d3a6fba8
Leimar Ocampo <bocampo@unal.edu.co>
2023-04-03 20:54:00 -0500

    hola

--- pregunta.py
+++ pregunta.py
@@ -1,28 +1,17 @@
-import re
 import pandas as pd
-
 def clean_data():
 
-    df = pd.read_csv("./solicitudes_credito.csv", sep=";", index_col = 0)
+    datafrem = pd.read_csv("solicitudes_credito.csv", sep=";")
+    datafrem.dropna(axis = 0, inplace = True)
+    datafrem.drop_duplicates(inplace = True)
+    datafrem=datafrem.drop(['Unnamed: 0'], axis=1)
+    datafrem[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]]=datafrem[["sexo", "tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]].apply(lambda x: x.astype(str).str.lower())
+    datafrem=datafrem.replace(to_replace="(_)|(-)",value=" ",regex=True)    
+    datafrem=datafrem.replace(to_replace="[,$]|(\.00$)",value="",regex=True)
+    datafrem.monto_del_credito = datafrem.monto_del_credito.astype("int")
+    datafrem.comuna_ciudadano = datafrem.comuna_ciudadano.astype("float")
+    datafrem.fecha_de_beneficio = pd.to_datetime(datafrem.fecha_de_beneficio,infer_datetime_format=True,errors='ignore',dayfirst=True)
+    datafrem.fecha_de_beneficio = datafrem.fecha_de_beneficio.dt.strftime("%Y/%m/%d")
+    datafrem.drop_duplicates(inplace = True)
 
-    df.dropna(axis=0, inplace=True)
-
-    df.monto_del_credito = df.monto_del_credito.str.strip("$")
-    df.monto_del_credito = df.monto_del_credito.str.replace(",","")
-    df.monto_del_credito = df.monto_del_credito.astype(float)
-    df.monto_del_credito = df.monto_del_credito.astype(int)
-    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
-
-    for filas in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
-        df[filas] = df[filas].str.lower()
-        df[filas] = df[filas].str.replace('_', ' ')
-        df[filas] = df[filas].str.replace('-', ' ')
-
-    df.drop_duplicates(inplace=True)
-
-    return df
-
-if __name__ == "__main__":
-    df = clean_data()
-    print(df.dtypes)
-    print(df.sexo.value_counts())
+    return datafrem
