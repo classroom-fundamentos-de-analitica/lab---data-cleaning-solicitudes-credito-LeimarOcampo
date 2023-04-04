e9699b0931cd814e66ee7f7f530ace4c2636cb2a
Leimar Ocampo <bocampo@unal.edu.co>
2023-04-03 20:51:03 -0500

    funcion

--- pregunta.py
+++ pregunta.py
@@ -3,26 +3,26 @@ import pandas as pd
 
 def clean_data():
 
-    datafrem = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
+    df = pd.read_csv("solicitudes_credito.csv", sep=";", index_col = 0)
 
-    datafrem.dropna(axis=0, inplace=True)
-    datafrem.monto_del_credito = datafrem.monto_del_credito.str.strip("$")
-    datafrem.monto_del_credito = datafrem.monto_del_credito.str.replace(",","")
-    datafrem.monto_del_credito = datafrem.monto_del_credito.astype(float)
-    datafrem.monto_del_credito = datafrem.monto_del_credito.astype(int)
+    df.dropna(axis=0, inplace=True)
 
-    datafrem.fecha_de_beneficio = pd.to_datetime(datafrem.fecha_de_beneficio, dayfirst=True)
+    df.monto_del_credito = df.monto_del_credito.str.strip("$")
+    df.monto_del_credito = df.monto_del_credito.str.replace(",","")
+    df.monto_del_credito = df.monto_del_credito.astype(float)
+    df.monto_del_credito = df.monto_del_credito.astype(int)
+    df.fecha_de_beneficio = pd.to_datetime(df.fecha_de_beneficio, dayfirst=True)
 
     for filas in ['sexo', 'tipo_de_emprendimiento', 'idea_negocio', 'línea_credito', 'barrio']:
-        datafrem[filas] = datafrem[filas].str.lower()
-        datafrem[filas] = datafrem[filas].str.replace('_', ' ')
-        datafrem[filas] = datafrem[filas].str.replace('-', ' ')
+        df[filas] = df[filas].str.lower()
+        df[filas] = df[filas].str.replace('_', ' ')
+        df[filas] = df[filas].str.replace('-', ' ')
 
-    datafrem.drop_duplicates(inplace=True)
+    df.drop_duplicates(inplace=True)
 
-    return datafrem
+    return df
 
 if __name__ == "__main__":
-    datafrem = clean_data()
-    print(datafrem.dtypes)
-    print(datafrem.sexo.value_counts())
+    df = clean_data()
+    print(df.dtypes)
+    print(df.sexo.value_counts())
